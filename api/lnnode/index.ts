ac60dc82f328e9fbbf7f6ece0324299aff3b5383
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-04-21 21:41:25 +0200

    Fixed unsupported url by env.
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- api/lnnode/index.ts
+++ api/lnnode/index.ts
@@ -1,8 +1,13 @@
 import type { APIRequest } from "aleph/types.ts";
 
 export default async function handler(req: APIRequest) {
+  let restUrl = Deno.env.get("REST_URL");
+  if (restUrl == null) {
+    restUrl = "localhost:7000";
+    console.log("ENV variable not found");
+  }
   const getInfoNode =
-    await (await fetch("http://localhost:7000/utility/getinfo")).json();
+    await (await fetch(`http://${restUrl}/utility/getinfo`)).json();
   console.error(getInfoNode);
   if (getInfoNode === undefined) {
     req.status(500);
