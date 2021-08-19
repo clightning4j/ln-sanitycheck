0beddfb7ec010dcfa6a3a2aae683c5730340132d
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-08-19 12:32:08 +0200

    Introduce again css.
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # deno run --allow-read --allow-net --allow-env --allow-write --allow-run https://deno.land/x/aleph@v0.3.0-alpha.31/cli.ts dev
-NO_WORRY = aleph start
+NO_WORRY=aleph start
 cp utils.js .aleph/production/-/cdn.esm.sh/v45/@material-ui/utils@4.11.2/deno
 aleph start
